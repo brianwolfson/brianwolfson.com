cfb07f9e07aff3e69a4930bcb4ba0c19a25f1e4b
brianwolfson <brian@brianwolfson.com>
2022-08-12 14:50:50 -0400

    1

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+/**
+* Template Name: MyResume - v4.8.1
+* Template URL: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function() {
+  "use strict";
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
+
+  /**
+   * Initiate Pure Counter
+   */
+  new PureCounter();
+
+})()
